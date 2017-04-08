e428e01dce11e19b18eac39e98efe02580ca554c
weichao66666 <weichao66666@gmail.com>
2017-04-08 10:39:05 +0800

    Initial commit.

---
+++ app/src/main/java/io/weichao/mult_pages/adapter/MultPagesAdapter.java
@@ -0,0 +1,33 @@
+package io.weichao.mult_pages.adapter;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+import io.weichao.mult_pages.R;
+import io.weichao.util.BitmapUtil;
+
+public class MultPagesAdapter {
+    private Activity mActivity;
+    private ArrayList<Integer> mImageLists;
+
+    public MultPagesAdapter(Activity activity) {
+        mActivity = activity;
+        mImageLists = new ArrayList<>();
+        mImageLists.add(R.mipmap.a);
+        mImageLists.add(R.mipmap.b);
+        mImageLists.add(R.mipmap.c);
+        mImageLists.add(R.mipmap.d);
+        mImageLists.add(R.mipmap.e);
+    }
+
+    public int getCount() {
+        return mImageLists.size();
+    }
+
+
+    public Bitmap getImage(int position) {
+        return BitmapUtil.getBitmap(mActivity, mImageLists.get(position));
+    }
+}
